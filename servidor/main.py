e81301507dc00022cdf216f3d62217273fac7e99
Jesus <galorasd@gmail.com>
2022-07-22 19:08:38 +0200

    Reestructurando servidor.

---
+++ servidor/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from socket import socket, error
+from threading import Thread
+from clases import Cliente
+import constantes
+
+def main():
+    s = socket()
+    
+    # Escuchar peticiones en el puerto puerto.
+    s.bind((constantes.Escuchar, constantes.Puerto))
+    s.listen(0)
+    
+    while True:
+        conexion, direccion = s.accept()
+        c = Cliente.Cliente(conexion, direccion)
+        c.start()
+        
+
+
+if __name__ == "__main__":
+    main()
