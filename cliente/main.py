0f6e1b33c2db3f9319d978eba2c4c7ce569a7bf3
Jesus <galorasd@gmail.com>
2022-07-22 20:10:41 +0200

    Añadida prueba con pygame.

--- cliente/main.py
+++ cliente/main.py
@@ -1,1 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from socket import socket
 import constantes
+from cytolk import tolk
+tolk.load()
+import sys, pygame
+
+try:
+    raw_input
+except NameError:
+    raw_input = input
+
+
+def main():
+    pygame.init()
+    ventana = pygame.display.set_mode(constantes.TamanioVentana)
+    pygame.display.set_caption(constantes.TituloVentana)
+
+    s = socket()
+    s.connect((constantes.Direccion, constantes.Puerto))
+    ejecutar=True
+    while ejecutar:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                ejecutar = False
+        teclas = pygame.key.get_pressed()
+        if teclas[pygame.K_UP]:
+            s.send(bytes('Hola, mundo','UTF-8'))
+            # try:
+                # s.send(output_data)
+            # except TypeError:
+                # s.send(bytes(output_data, "utf-8"))
+            
+            # Recibir respuesta.
+            datos_recibidos = s.recv(1024)
+            if datos_recibidos:
+                datos = datos_recibidos.decode("utf-8")
+                tolk.output("Verbalizo: "+datos)
+    exit()
+
+
+if __name__ == "__main__":
+    main()
